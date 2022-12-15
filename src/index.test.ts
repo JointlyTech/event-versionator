828e118b91224ea3b59b0e67e6ed209790dc7ce8
Michael Di Prisco <cadienvan@gmail.com>
2022-12-15 15:49:35 +0100

    BREAKING CHANGE: changed functions input parameters + improved type checkings

--- src/index.test.ts
+++ src/index.test.ts
@@ -5,14 +5,14 @@ import {
 } from './lib';
 
 beforeEach(() => {
-  getNextEventVersion(EVENT_VERSIONING_RESET_STRING);
+  getNextEventVersion({ event: EVENT_VERSIONING_RESET_STRING });
 });
 
 it('should get the next event version in sequence', () => {
-  expect(getNextEventVersion('a')).toBe(1);
-  expect(getNextEventVersion('a')).toBe(2);
-  expect(getNextEventVersion('b')).toBe(1);
-  expect(getNextEventVersion('a')).toBe(3);
+  expect(getNextEventVersion({ event: 'a' })).toBe(1);
+  expect(getNextEventVersion({ event: 'a' })).toBe(2);
+  expect(getNextEventVersion({ event: 'b' })).toBe(1);
+  expect(getNextEventVersion({ event: 'a' })).toBe(3);
 });
 
 it('should return version 1 for a first-time event', () => {
@@ -20,18 +20,27 @@ it('should return version 1 for a first-time event', () => {
 });
 
 it('should return 1 for a first-time event if commit is false', () => {
-  expect(getNextEventVersion('test', false)).toBe(1);
+  expect(getNextEventVersion({ event: 'test', commit: false })).toBe(1);
 });
 
 it('should return 2 for a second-time event if committed twice', () => {
-  expect(getNextEventVersion('test', false)).toBe(1);
-  expect(getNextEventVersion('test', true)).toBe(1);
-  expect(getNextEventVersion('test', true)).toBe(2);
+  expect(getNextEventVersion({ event: 'test', commit: false })).toBe(1);
+  expect(getNextEventVersion({ event: 'test', commit: true })).toBe(1);
+  expect(getNextEventVersion({ event: 'test', commit: true })).toBe(2);
 });
 
 it('should return 2 for a second-time event if committed once', () => {
-  expect(getNextEventVersion('test', false)).toBe(1);
-  expect(getNextEventVersion('test', true)).toBe(1);
-  expect(getNextEventVersion('test', false)).toBe(2);
-  expect(getNextEventVersion('test', false)).toBe(2);
+  expect(getNextEventVersion({ event: 'test', commit: false })).toBe(1);
+  expect(getNextEventVersion({ event: 'test', commit: true })).toBe(1);
+  expect(getNextEventVersion({ event: 'test', commit: false })).toBe(2);
+  expect(getNextEventVersion({ event: 'test', commit: false })).toBe(2);
+});
+
+it('should return the correct version for an empty event', () => {
+  expect(getVersionedEvent().version).toBe(1);
+  expect(getVersionedEvent().version).toBe(2);
+  expect(getNextEventVersion()).toBe(3);
+  expect(getNextEventVersion()).toBe(4);
+  expect(getNextEventVersion({ commit: false })).toBe(5);
+  expect(getNextEventVersion({ commit: false })).toBe(5);
 });
--- src/lib.ts
+++ src/lib.ts
@@ -1,8 +1,34 @@
 export const EVENT_VERSIONING_RESET_STRING = 'EVENT_VERSIONING_RESET_STRING';
 
+type EventVersionatorInput = {
+  event: string;
+  commit: boolean;
+};
+
+const defaultEventVersionatorOptions: EventVersionatorInput = {
+  event: '',
+  commit: true
+};
+
+export type VersionedEvent = InputEvent & {
+  version: number;
+  createdAt: Date;
+  payload: unknown;
+};
+
+export type InputEvent = {
+  event: string;
+  payload?: unknown;
+};
+
 export const getNextEventVersion = (() => {
   let internalVersioning = {};
-  return (event: string, commit = true): number => {
+
+  return (options?: Partial<EventVersionatorInput>): number => {
+    const { event, commit }: EventVersionatorInput = {
+      ...defaultEventVersionatorOptions,
+      ...(options || {})
+    };
     if (EVENT_VERSIONING_RESET_STRING === event) {
       internalVersioning = {};
       return 0;
@@ -15,20 +41,12 @@ export const getNextEventVersion = (() => {
   };
 })();
 
-export type VersionedEvent = {
-  event: string;
-  payload: unknown;
-  version: number;
+export const getVersionedEvent = (options?: InputEvent): VersionedEvent => {
+  options = options || { event: '' };
+  return {
+    event: options?.event,
+    version: getNextEventVersion({ event: options?.event, commit: true }),
+    payload: options?.payload || null,
+    createdAt: new Date()
+  };
 };
-
-export const getVersionedEvent = ({
-  event,
-  payload
-}: {
-  event: string;
-  payload: unknown;
-}): VersionedEvent => ({
-  event,
-  version: getNextEventVersion(event, true),
-  payload
-});
